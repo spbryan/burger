d15095001b398b0a45fdc2aa820e2133df9b73c8
spbryan <seanbryan@comcast.com>
2019-06-26 18:13:17 -0400

    define express details in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /***********************************************
  * Project: Burger
  * Developer: Sean Bryan
- * Date: 2019-06-22
+ * Date: 2019-06-26
  ***********************************************/
 //Dependencies
 var express = require("express");
@@ -9,12 +9,18 @@ var express = require("express");
 //Set up Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //ROUTER
-// require('./app/routing/apiRoutes')(app); 
-// require('./app/routing/htmlRoutes')(app); 
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 // Listener
 app.listen(PORT, function () {
