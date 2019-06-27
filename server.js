c30c9400e73eed1671d30fbb8d69e25cc3ea280a
spbryan <seanbryan@comcast.com>
2019-06-27 18:11:00 -0400

    fix issue for pulling in local css file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 // app.use(express.static("public"));
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
